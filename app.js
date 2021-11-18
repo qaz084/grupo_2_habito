88613f07a341cd5e2d86f3366a165a344ef7aa5c
qaz084 <jonathaniurman@gmail.com>
2021-11-18 17:51:04 -0300

    creacion de repositorio, estructuras de carpetas, instalacion node & express, requires express, path, creacion  app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+//---Require express & path----
+
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//Inicializando servidor en puerto a definir
+
+// Definir puerto
+// const numeroPuerto= 90000;
+// app.listen(numeroPuerto,()=>console.log("Servidor montado en Puerto "+numeroPuerto));
