e95648b4c86ec35499270755de19c2e0f490dd00
modoliso3k007 <rodriazteca156@gmail.com>
2021-12-01 19:03:26 -0300

    Se creo el servidor y los archivos html y css

--- app.js
+++ app.js
@@ -4,8 +4,30 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-//Inicializando servidor en puerto a definir
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
 
-// Definir puerto
-// const numeroPuerto= 90000;
-// app.listen(numeroPuerto,()=>console.log("Servidor montado en Puerto "+numeroPuerto));
+app.listen(3000, ()=>{
+    console.log("Servidor corriendo")
+});
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+});
+
+app.get("/productDetail",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
+});
+
+app.get("/productCart",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
+});
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
