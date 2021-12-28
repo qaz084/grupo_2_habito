acb04dae91fe0c309b227f9e2ed821deef3b435e
nahuelSToledo <nahueltoledo5@gmail.com>
2021-12-28 19:09:16 -0300

    creando carpeta src,product,users,partials y routes

--- app.js
+++ app.js
@@ -7,27 +7,28 @@ const path = require("path");
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("Servidor corriendo")
 });
 
-app.get("/",(req,res)=>{
+app.use('view engine', 'ejs');
+
+app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 });
 
-app.get("/productDetail",(req,res)=>{
+app.get("/productDetail", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
 });
 
-app.get("/productCart",(req,res)=>{
+app.get("/productCart", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
 });
 
-app.get("/register",(req,res)=>{
+app.get("/register", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/register.html"))
 });
 
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
 });
-
