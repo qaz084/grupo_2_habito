13365cb9ed7dfb99e4f07c00a94e90061962c23c
NahuelSToledo <nahueltoledo5@gmail.com>
2022-02-04 00:17:09 -0300

    validacion login y register

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,28 +1,38 @@
 const fs = require('fs');
 const path = require('path');
-
+const { validationResult } = require('express-validator');
 //Ubicar archivo Json
 const filePath = path.resolve(__dirname, '../data/users.json');
 //Lectura de Json y Parseo
 const usersArray = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
 const generateID = () => {
-    if(usersArray.length != 0){       
-        const lastProduct =  usersArray[Number(usersArray.length) - Number(1)];	         
-        const lastID = Number(lastProduct.id) + Number(1);	
+    if (usersArray.length != 0) {
+        const lastProduct = usersArray[Number(usersArray.length) - Number(1)];
+        const lastID = Number(lastProduct.id) + Number(1);
         return lastID;
-    }else{
+    } else {
         const lastID = 1
         return lastID;
     }
 };
 
 const controller = {
-    userLogin:(req,res)=>{
+    userLogin: (req, res) => {
         return res.render("../views/users/login")
     },
 
     logUser: (req, res) => {
+        const resultValidation = validationResult(req);
+        if (resultValidation.errors.length > 0) {
+            return res.render("../views/users/login", {
+                errors: resultValidation.mapped(),
+                oldData: req.body,
+            })
+        } else {
+            return res.render("../views/users/profile")
+        }
+
         const email = req.body.email;
         const pass = req.body.contrasenia;
         usersArray.forEach(usuario => {
@@ -31,13 +41,23 @@ const controller = {
             } else {
                 res.redirect('/users/login?logged=false');
             }
-        }
-    )},
-    
-    userRegister:(req,res)=>{
+        })
+    },
+
+    userRegister: (req, res) => {
+
         return res.render("../views/users/register2")
     },
     addUser: (req, res) => {
+        const resultValidation = validationResult(req);
+        if (resultValidation.errors.length > 0) {
+            return res.render("../views/users/register2", {
+                errors: resultValidation.mapped(),
+                oldData: req.body,
+            })
+        } else {
+            return res.render("../views/index")
+        }
         usersArray.push({
             nombre: req.body.nombre,
             apellido: req.body.apellido,
@@ -46,12 +66,12 @@ const controller = {
             repetirPass: req.body.repetirContraseña,
             imagen: req.file.filename,
             id: generateID()
-        });  
+        });
 
         fs.writeFileSync(filePath, JSON.stringify(usersArray, null, ' '))
         res.redirect('/?saved=true');
     },
-    
+
 };
 
 module.exports = controller;
--- src/routes/users.js
+++ src/routes/users.js
@@ -14,20 +14,62 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({storage});
+const upload = multer({ storage });
 
 const controller = require("../controllers/usersController");
+const { body } = require('express-validator');
+
+const validationLogin = [
+    body('email').notEmpty().withMessage('Debe ingresar un email').bail()
+    .isEmail().withMessage('Debe ingresar un Email valido'),
+
+    body('contrasenia').notEmpty().withMessage('Debe ingresar la contraseña').bail()
+    .isLength({ min: 5 }).withMessage('La contraseña debe ser mas larga')
+]
+const validationRegister = [
+    body('nombre').notEmpty().withMessage('Debe ingresar un nombre'),
+
+    body('apellido').notEmpty().withMessage('Debe ingresar un apellido'),
+
+    body('email').notEmpty().withMessage('Debe ingresar un email').bail()
+    .isEmail().withMessage('Debe ingresar un Email valido'),
+
+    body('contraseña').notEmpty().withMessage('Debe ingresar la contraseña').bail()
+    .isLength({ min: 5 }).withMessage('La contraseña debe ser mas larga'),
+
+    body('repetirContraseña').notEmpty().withMessage('Debe ingresar la contraseña').bail()
+    .isLength({ min: 5 }).withMessage('La contraseña debe ser mas larga'),
+
+    body('imagen').custom((value, { req }) => {
+        let file = req.file;
+        let extensionAccepted = ['.jpg', '.png'];
+        if (!file) {
+            throw new Error('Tienes que subir una imagen');
+
+        } else {
+            let extensionFile = path.extname(file.originalname);
+            if (!extensionAccepted.includes(extensionFile)) {
+                throw new Error('Debe cargar una imagen en formato .jpg o .png');
+
+            }
+        }
+        return true;
+    })
+]
+
+
+
 
 
 //GET - http://localhost3000/users/login
 router.get("/login", controller.userLogin);
 //POST - http://localhost3000/users/login
-router.post("/login", controller.logUser);
+router.post("/login", validationLogin, controller.logUser);
 
 
 //GET - http://localhost3000/users/register
 router.get("/register2", controller.userRegister);
 //POST - http://localhost3000/users
-router.post("/", upload.single('imagen'),controller.addUser);
+router.post("/", upload.single('imagen'), validationRegister, controller.addUser);
 
 module.exports = router;
