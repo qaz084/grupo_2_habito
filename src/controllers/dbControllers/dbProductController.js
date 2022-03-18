ce1703b691082ba71d698cf09f0680223bea07a5
modoliso3k007 <rodriazteca156@gmail.com>
2022-03-18 15:07:49 -0300

    funcion create completada

--- src/controllers/dbControllers/dbProductController.js
+++ src/controllers/dbControllers/dbProductController.js
@@ -29,33 +29,37 @@ const controller={
 				colors[i].name = colors[i].name.split("").slice(1).join("")
 			}
 			res.render("../views/products/createProduct",{category : categories, color : colors, size : size})
-	} catch (err) {
-		console.log(err)
-	}
+		} catch (err) {
+			console.log(err)
+		}
 	},
 	add :async (req, res) => {
-		let a = Object.keys(req.body).filter((element)=>{
-			return element.length<=2
-		}) 
-		for ( let i = 0;  i <a.length;i++){
-			a[i] = a[i].split("").slice(1).join("")
+		try{
+			let a = Object.keys(req.body).filter((element)=>{
+				return element.length<=2
+			}) 
+			for ( let i = 0;  i <a.length;i++){
+				a[i] = a[i].split("").slice(1).join("")
+			}
+			console.log(a)
+			const newProduct = await Product.create({
+				name: req.body.productName,
+				price: req.body.productPrice,
+				discount: req.body.productDiscount,
+				description: req.body.productDescription,
+				quantity: 10,
+				image1: req.files.image1? req.files.image1[0].filename : "default-image.png",
+				image2: req.files.image2? req.files.image[0].filename : "default-image.png",
+				image3: req.files.image3? req.files.image[0].filename : "default-image.png",
+				image4: req.files.image4? req.files.image[0].filename : "default-image.png",
+				categoryId: req.body.category
+			});
+			await newProduct.addColor(a)
+			await newProduct.addSize(req.body.size)
+			return res.redirect("/") 
+		}catch (err) {
+			console.log(err)
 		}
-
-		const newProduct = await Product.create({
-			name: req.body.productName,
-			price: req.body.productPrice,
-			discount: req.body.productDiscount,
-			description: req.body.productDescription,
-			quantity: 10,
-			image1: req.files.image1? req.files.image1[0].filename : "default-image.png",
-			image2: req.files.image2? req.files.image[0].filename : "default-image.png",
-			image3: req.files.image3? req.files.image[0].filename : "default-image.png",
-			image4: req.files.image4? req.files.image[0].filename : "default-image.png",
-			categoryId: req.body.category
-		});
-		newProduct.addColor(a).then(()=>{});
-		newProduct.addSize(req.body.size)
-		.then(()=>{res.redirect("/")})
 	}, 
 
 	editProduct:(req,res)=>{
