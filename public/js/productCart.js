b92ab198139a8d2f1d338c62d5c454ddd9254ca1
qaz084 <jonathaniurrman@gmail.com>
2022-03-23 18:29:06 -0300

    detalles vistas, logout navbar, comienzo diagrama nuevo cart

---
+++ public/js/productCart.js
@@ -0,0 +1,16 @@
+console.log('FUNCA');
+
+
+let productsArray=[];
+
+
+
+let btnAddCart = document.querySelector('.btn-addCart');
+
+btnAddCart.addEventListener('click',()=>{
+
+    productsArray.push(btnAddCart);
+})
+
+// locals.productCart=productsArray;
+
