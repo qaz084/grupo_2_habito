9d4629c9066a29604c0754e74b6a3d877d7e86b2
qaz084 <jonathaniurrman@gmail.com>
2022-04-14 00:17:04 -0300

    api products

---
+++ src/controllers/dbControllers/api/apiProducts.js
@@ -0,0 +1,56 @@
+const { Product, Category } = require('../../../database/models');
+
+module.exports = {
+
+    detail: async (req, res) => {
+        
+        let productDetail = await Product.findOne({
+            include: ["size", "category",'color'],
+            where: {
+                id: req.params.id
+            },
+        });
+        
+      
+
+         productDetail.dataValues.image1 = 'http://localhost:3001/uploads/products/'+ productDetail.image1;
+
+        return res.json({
+
+           productData: productDetail,
+           img: productDetail.image1
+
+        })
+    },
+
+    show: async (req, res) => {
+
+        const productsCategories= await Category.findAll({
+
+
+
+            include: ["product"],
+
+
+
+        });
+        // products.forEac
+
+
+        const products= await Product.findAll({
+
+            include: ["category"],
+
+            attributes: ['id', 'name', 'description']
+
+
+        });
+        // products.forEach(oneProduct => {oneProduct.dataValues.detail = 'api/Products/' + oneProduct.id})
+          
+        return res.json({
+            count: products.length,
+            Products: products,
+            countByCategory:productsCategories
+        })
+    }
+}
--- src/routes/api/apiRoutes.js
+++ src/routes/api/apiRoutes.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../../controllers/dbControllers/api/apiUsers');
+ const apiProductsController = require('../../controllers/dbControllers/api/apiProducts');
 
 
 // hhtp://localhost:3000/api/users
 router.get('/users', userController.show);
 
 router.get('/users/:id', userController.detail);
+
+ router.get('/products', apiProductsController.show);
+
+ router.get('/products/:id', apiProductsController.detail);
 
 module.exports = router;
